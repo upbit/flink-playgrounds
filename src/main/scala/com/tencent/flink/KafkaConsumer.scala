9c7cc40766c0574d564196693b9fda0b27f288c6
Hao Zhou <deryzhou@tencent.com>
2019-11-22 22:09:06 +0800

    fix: Fix docker version, new pom for flink-1.9

---
+++ src/main/scala/com/tencent/flink/KafkaConsumer.scala
@@ -0,0 +1,54 @@
+package com.tencent.flink
+
+import java.util.Properties
+
+import org.apache.flink.api.scala._
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.api.java.utils.ParameterTool
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala.{DataStream, StreamExecutionEnvironment}
+import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+
+object KafkaConsumer {
+  def main(args: Array[String]) {
+    val parameter = ParameterTool.fromArgs(args)
+
+    // set up the streaming execution environment
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
+    env.enableCheckpointing(5000)
+
+    val properties = new Properties()
+    val bootstrap_servers = parameter.get("bootstrap_servers", "zookeeper:2181")
+    val group_id = parameter.get("group_id", "oceanus-playground")
+
+    // topic
+    val topic: String = try {
+      parameter.get("topic")
+    } catch {
+      case e: Exception => {
+        System.err.println("No topic specified. Please run 'KafkaConsumer --topic <topic>'")
+        return
+      }
+    }
+
+    properties.setProperty("bootstrap.servers", bootstrap_servers)
+    properties.setProperty("group.id", group_id)
+
+    val consumer = new FlinkKafkaConsumer[String](topic, new SimpleStringSchema(), properties)
+    // 方式一：从头开始消费，这种方式会导致重复消费
+    // consumer.setStartFromEarliest()
+    // 方式二：忽略kafka已有的消息，从最新的位置消费，该方式会导致有些消息没被消费
+    // consumer.setStartFromLatest()
+    // 方式三：指定时间往后消费,注意：时间不能>=当前时间
+    // consumer.setStartFromTimestamp(System.currentTimeMillis()-1000*60*60)
+    consumer.setCommitOffsetsOnCheckpoints(true)
+
+    val stream: DataStream[String] = env.addSource(consumer)
+    stream.print()
+
+    // execute program
+    env.execute("KafkaConsumer")
+  }
+}
